91b89643650327e20680f42be9f1a63d1b377392
esbena <esbena@esbena-ThinkPad-R61>
2012-08-17 17:40:53 -0700

    initial model syncable (also: eclipse files)

--- website/mosaicportfolio/models.py
+++ website/mosaicportfolio/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+class SiteKind:
+    github = 1
+    values = [(github, github)]
+    
+class UserSite(models.Model):
+    user = models.ForeignKey(User)
+    login = models.CharField(max_length=200)
+    kind = models.IntegerField(choices=SiteKind.values)
+    
+class Project(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class ProjectRepository(models.Model):
+    project = models.ForeignKey(Project)
+    login = models.CharField(max_length=200)
+    repository = models.ForeignKey(Repository)
+
+class ProjectWiki(models.Model):
+    project = models.ForeignKey(Project)
+    login = models.CharField(max_length=200)
+    wiki = models.ForeignKey(Wiki)
+
+class ProjectIssueTracker(models.Model):
+    project = models.ForeignKey(Project)
+    login = models.CharField(max_length=200)
+    issue_tracker = models.ForeignKey(IssueTracker)
+
+class RepositoryKind:
+    git = 1
+    values = [(git, git)]
+    
+class Repository(models.Model):
+    url = models.URLField()
+    kind = models.IntegerField(choices=RepositoryKind.values)
+    class Meta:
+        verbose_name_plural = "repositories"
+
+class WikiKind:
+    github = 1
+    values = [(github, github)]
+    
+class Wiki(models.Model):
+    url = models.URLField()
+    kind = models.IntegerField(choices=WikiKind.values)
+
+class IssueTrackerKind:
+    github = 1
+    values = [(github, github)]
+    
+class IssueTracker(models.Model):
+    url = models.URLField()
+    kind = models.IntegerField(choices=IssueTrackerKind.values)
