fc5892dbaed45565263a5fb4b4dfbf11626d8fa3
Casper S. Jensen <casper@svenningjensen.dk>
2012-08-17 22:15:24 -0700

    Syntax

--- website/mosaicportfolio/models.py
+++ website/mosaicportfolio/models.py
@@ -20,8 +20,10 @@ class IssueTrackerKind:
 class Repository(models.Model):
     url = models.CharField(max_length=200)
     kind = models.IntegerField(choices=RepositoryKind.values)
+
     class Meta:
         verbose_name_plural = "repositories"
+
     def __str__(self):
         return "%s(%s)" % (self.url, self.kind)
     
@@ -31,12 +33,14 @@ class Repository(models.Model):
 class Wiki(models.Model):
     url = models.CharField(max_length=200)
     kind = models.IntegerField(choices=WikiKind.values)
+
     def __str__(self):
         return "%s(%s)" % (self.url, self.kind)
     
 class IssueTracker(models.Model):
     url = models.CharField(max_length=200)
     kind = models.IntegerField(choices=IssueTrackerKind.values)
+
     def __str__(self):
         return "%s(%s)" % (self.url, self.kind)
 
@@ -44,6 +48,7 @@ class UserSite(models.Model):
     user = models.ForeignKey(User)
     login = models.CharField(max_length=200)
     kind = models.IntegerField(choices=SiteKind.values)
+
     def __str__(self):
         return "%s: %s@%s" % (self.user, self.login, self.kind)
     
@@ -51,6 +56,7 @@ class Project(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=200)
     description = models.TextField()
+
     def __str__(self):
         return "%s@%s" % (self.user, self.name)
      
@@ -58,8 +64,10 @@ class ProjectRepository(models.Model):
     project = models.ForeignKey(Project)
     login = models.CharField(max_length=200)
     repository = models.ForeignKey(Repository)
+
     class Meta:
         verbose_name_plural = "project repositories"
+
     def __str__(self):
         return "%s@%s" % (self.login, self.repository) 
 
@@ -67,6 +75,7 @@ class ProjectWiki(models.Model):
     project = models.ForeignKey(Project)
     login = models.CharField(max_length=200)
     wiki = models.ForeignKey(Wiki)
+
     def __str__(self):
         return "%s@%s" % (self.login, self.wiki) 
 
@@ -74,34 +83,42 @@ class ProjectIssueTracker(models.Model):
     project = models.ForeignKey(Project)
     login = models.CharField(max_length=200)
     issue_tracker = models.ForeignKey(IssueTracker)
+
     def __str__(self):
         return "%s@%s" % (self.login, self.issue_tracker)
      
 class Activity(models.Model):
     date = models.DateTimeField()
     login = models.CharField(max_length=200)
+
     class Meta:
         abstract = True
         verbose_name_plural = "activities"
          
 class RepositoryActivity(Activity):
     repository = models.ForeignKey(Repository)
+
     class Meta:
         verbose_name_plural = "repository activities"
+
     def __str__(self):
         return "%s@%s(%s)" % (self.login, self.repository, self.date)
 
 class IssueTrackerActivity(Activity):
     issue_tracker = models.ForeignKey(IssueTracker)
+
     class Meta:
         verbose_name_plural = "issue tracker activities"
+
     def __str__(self):
         return "%s@%s(%s)" % (self.login, self.issue_tracker, self.date)
 
 class WikiActivity(Activity):
     wiki = models.ForeignKey(Wiki)
+
     class Meta:
         verbose_name_plural = "wiki activities"
+
     def __str__(self):
         return "%s@%s(%s)" % (self.login, self.wiki, self.date)
 
