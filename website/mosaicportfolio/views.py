c4307be3f1a94f669d6a8bdfa9aa90bf0501d1d6
Casper S. Jensen <casper@svenningjensen.dk>
2012-08-17 19:00:39 -0700

    Add blank structure of website

--- website/mosaicportfolio/views.py
+++ website/mosaicportfolio/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "mosaicportfolio/home.html")
+
+def portfolio(request, username):
+    return render(request, "mosaicportfolio/portfolio.html")
+
+def profile_edit(request):
+    return render(request, "mosaicportfolio/profile_edit.html")
